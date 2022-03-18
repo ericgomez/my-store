68ad20496ba3e9aa7d915a640366b46dbaf923bd
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-18 09:53:09 -0600

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const faker = require('faker')
 const app = express()
 const port = 3000
 
@@ -11,19 +12,27 @@ app.get('/new-route', (req, res) => {
   res.send('New route')
 })
 
+app.get('/products/filter', (req, res) => {
+  res.send('Filter products')
+})
+
+// http://localhost:3000/products?limit=10
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 10
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 20
-    }
-  ])
+  const products = []
+  const { limit } = req.query
+
+  const size = limit ? parseInt(limit) : 100
+
+  for (let i = 0; i < size; i++) {
+    products.push({
+      id: faker.random.uuid(),
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price()),
+      image: faker.image.imageUrl()
+    })
+  }
+
+  res.json(products)
 })
 
 app.get('/products/:id', (req, res) => {
@@ -34,6 +43,28 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 1',
     price: 10
   })
+})
+
+// http://localhost:3000/users?limit=10&offset=0
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+
+  if (limit && offset) {
+    res.json([
+      {
+        id: 1,
+        name: 'User 1',
+        age: 10
+      },
+      {
+        id: 2,
+        name: 'User 2',
+        age: 20
+      }
+    ])
+  } else {
+    res.status(400).send('Bad request')
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
