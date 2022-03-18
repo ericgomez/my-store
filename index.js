c6977e44e5161e3dbde813b3deec1aae56216e76
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-18 09:30:06 -0600

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -12,8 +12,36 @@ app.get('/new-route', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 10
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 20
+    }
+  ])
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+
   res.json({
-    id: 1,
+    id: id,
+    name: 'Product 1',
+    price: 10
+  })
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+
+  res.json({
+    categoryId: categoryId,
+    productId: productId,
     name: 'Product 1',
     price: 10
   })
