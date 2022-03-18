b50c2a64400e95e7fb624c6e502b84ae990f2980
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-18 09:19:35 -0600

    Routing con Express.js

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/new-route', (req, res) => {
+  res.send('New route')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Product 1',
+    price: 10
+  })
+})
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
