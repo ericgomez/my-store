2068a497b829e8b18492d42ff965534d6446e010
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-18 13:26:01 -0600

    Validación de datos con Joi

---
+++ middlewares/validator.handler.js
@@ -0,0 +1,21 @@
+const Boom = require('@hapi/boom')
+
+function validatorHandler (schema, property) {
+  // closure
+  return (req, res, next) => {
+    /*
+     Dynamic request req[property] = req.body or req.query or req.params
+     */
+    const { error } = schema.validate(req[property])
+
+    if (error) {
+      // send middleware error - Boom error
+      next(Boom.badRequest(error.details[0].message))
+    } else {
+      // continue
+      next()
+    }
+  }
+}
+
+module.exports = validatorHandler
